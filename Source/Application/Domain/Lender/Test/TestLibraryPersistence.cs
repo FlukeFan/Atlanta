a2129a7a857905fad571eee2389cea13a25a6475
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2007-01-15 17:47:50 +0000

    Added persistence for Library Media (media is already plural)

---
+++ Source/Application/Domain/Lender/Test/TestLibraryPersistence.cs
@@ -0,0 +1,45 @@
+
+using System;
+
+using NHibernate;
+using NHibernate.Cfg;
+
+using NUnit.Framework;
+
+using Atlanta.Application.Domain.DomainBase.Test;
+
+namespace Atlanta.Application.Domain.Lender.Test
+{
+
+    [TestFixture]
+    public class TestLibraryPersistence : DomainTestBase
+    {
+
+        [Test]
+        public void Insert_Load_Ok()
+        {
+            Library library = Library.InstantiateLibrary();
+
+            Session.Save(library);
+            Assert.IsTrue(library.Id != 0);
+
+            Session.Flush();
+            Session.Clear();
+
+            library = (Library) Session.Load(typeof(Library), library.Id);
+
+            Assert.AreEqual(0, library.Media.Count);
+        }
+
+        [Test]
+        public void LoadCheckAggregates_Ok()
+        {
+            Library library = (Library) Session.Load(typeof(Library), 1L);
+
+            Assert.AreEqual(3, library.Media.Count);
+        }
+
+    }
+
+}
+
