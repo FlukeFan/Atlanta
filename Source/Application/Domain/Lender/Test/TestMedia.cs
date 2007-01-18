afc55e8b0d6773a1fb457cba8e264d72fde386b2
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2007-01-18 18:02:45 +0000

    Fleshed out tests to demonstrate use of the custom domain criteria

--- Source/Application/Domain/Lender/Test/TestMedia.cs
+++ Source/Application/Domain/Lender/Test/TestMedia.cs
@@ -42,16 +42,39 @@ namespace Atlanta.Application.Domain.Lender.Test
         {
             IList<Media> mediaList = new DomainList<Media>();
 
-            mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Cd, "test name 1", "test description"));
-            mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Cd, "test name 2", "test description"));
-            mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Cd, "test name 3", "test description"));
+            mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Cd, "test name 1",   "test description"));
+            mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Cd, "test name 2.1", "test description"));
+            mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Cd, "test name 2.2", "test description"));
 
-            IList<Media> filteredList = new MediaCriteria()
-                                            .SetNameFilter("test name 2")
-                                            .List(mediaList);
+            IList<Media> filteredList;
+            {
+                filteredList = new MediaCriteria()
+                                    .SetNameFilter("test name 2.1")
+                                    .List(mediaList);
 
-            Assert.AreEqual(1, filteredList.Count);
-            Assert.AreEqual("test name 2", filteredList[0].Name);
+                Assert.AreEqual(1, filteredList.Count);
+                Assert.AreEqual("test name 2.1", filteredList[0].Name);
+            }
+
+            {
+                filteredList = new MediaCriteria()
+                                    .SetNameFilter("test name 2.1", FilterCondition.NotEqual)
+                                    .List(mediaList);
+
+                Assert.AreEqual(2, filteredList.Count);
+                Assert.AreEqual("test name 1", filteredList[0].Name);
+                Assert.AreEqual("test name 2.2", filteredList[1].Name);
+            }
+
+            {
+                filteredList = new MediaCriteria()
+                                    .SetNameFilter("test name 2%", FilterCondition.Like)
+                                    .List(mediaList);
+
+                Assert.AreEqual(2, filteredList.Count);
+                Assert.AreEqual("test name 2.1", filteredList[0].Name);
+                Assert.AreEqual("test name 2.2", filteredList[1].Name);
+            }
         }
 
         [Test]
@@ -63,12 +86,25 @@ namespace Atlanta.Application.Domain.Lender.Test
             mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Book,    "test name", "test description"));
             mediaList.Add(Media.InstantiateOrphanedMedia(MediaType.Dvd,     "test name", "test description"));
 
-            IList<Media> filteredList = new MediaCriteria()
-                                            .SetTypeFilter(MediaType.Book)
-                                            .List(mediaList);
+            IList<Media> filteredList;
+            {
+                filteredList = new MediaCriteria()
+                                    .SetTypeFilter(MediaType.Book)
+                                    .List(mediaList);
 
-            Assert.AreEqual(1, filteredList.Count);
-            Assert.AreEqual(MediaType.Book, filteredList[0].Type);
+                Assert.AreEqual(1, filteredList.Count);
+                Assert.AreEqual(MediaType.Book, filteredList[0].Type);
+            }
+
+            {
+                filteredList = new MediaCriteria()
+                                    .SetTypeFilter(MediaType.Book, FilterCondition.NotEqual)
+                                    .List(mediaList);
+
+                Assert.AreEqual(2, filteredList.Count);
+                Assert.AreEqual(MediaType.Cd, filteredList[0].Type);
+                Assert.AreEqual(MediaType.Dvd, filteredList[1].Type);
+            }
         }
 
     }
