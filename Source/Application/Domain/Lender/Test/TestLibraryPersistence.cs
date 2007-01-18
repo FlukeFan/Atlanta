4f499ca467e8a7a7a54c2cc82e4201230da5679e
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2007-01-18 21:58:13 +0000

    Added extra tests to demonstrate criteria usage

--- Source/Application/Domain/Lender/Test/TestLibraryPersistence.cs
+++ Source/Application/Domain/Lender/Test/TestLibraryPersistence.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 using NUnit.Framework;
 
+using Atlanta.Application.Domain.DomainBase;
 using Atlanta.Application.Domain.DomainBase.Test;
 
 namespace Atlanta.Application.Domain.Lender.Test
@@ -50,6 +51,34 @@ namespace Atlanta.Application.Domain.Lender.Test
 
                 Assert.AreEqual(1, filteredList.Count);
                 Assert.AreEqual("CD", filteredList[0].Name);
+            }
+
+            {
+                filteredList = new MediaCriteria()
+                                    .SetTypeFilter(MediaType.Dvd)
+                                    .List(library.Media);
+
+                Assert.AreEqual(1, filteredList.Count);
+                Assert.AreEqual("DVD", filteredList[0].Name);
+            }
+
+            {
+                filteredList = new MediaCriteria()
+                                    .SetTypeFilter(MediaType.Dvd)
+                                    .SetNameFilter("CD")
+                                    .List(library.Media);
+
+                Assert.AreEqual(0, filteredList.Count);
+            }
+
+            {
+                filteredList = new MediaCriteria()
+                                    .SetTypeFilter(MediaType.Dvd, FilterCondition.NotEqual)
+                                    .List(library.Media);
+
+                Assert.AreEqual(2, filteredList.Count);
+                Assert.AreEqual("Book", filteredList[0].Name);
+                Assert.AreEqual("CD", filteredList[1].Name);
             }
         }
 
