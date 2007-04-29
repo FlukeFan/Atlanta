bdea183c39803f812e48731aa85ea13b1fee0988
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2007-04-29 12:07:31 +0000

    Added some structure for presentation build

---
+++ Source/Presentation/Lending/ConfigureMediaController.cs
@@ -0,0 +1,12 @@
+
+namespace Atlanta.Presentation.Lending
+{
+
+    /// <summary>
+    ///  Controller for ConfigureMedia view
+    /// </summary>
+    public class ConfigureMediaController
+    {
+    }
+
+}
