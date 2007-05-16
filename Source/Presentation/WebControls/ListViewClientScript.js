87fcdfda6c86e5bb0fac4289d687dbda7d1a4cf0
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2007-05-16 12:54:20 +0000

    Added event handler to prevent selection in lists (don't think FireFox supports this)

--- Source/Presentation/WebControls/ListViewClientScript.js
+++ Source/Presentation/WebControls/ListViewClientScript.js
@@ -1,12 +1,22 @@
 
+
+// onselectstart - disables selection by dragging in a list
+function AtlantaListView_OnSelectStart()
+{
+    event.returnValue = false;
+    event.cancelBubble = true;
+}
+
+
 // Initialises all Atlanta server ListView controls
 function InitialiseAtlantaListViewControls()
 {
     for (var listViewIndex in _atlantaListViewControls)
     {
         var listViewName = _atlantaListViewControls[listViewIndex];
-
         var listView = document.getElementById(listViewName);
+
+        listView.onselectstart = AtlantaListView_OnSelectStart;
     }
 }
 
