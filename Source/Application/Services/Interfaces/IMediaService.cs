71df33c254f4ce4dad00402686f5977455fe2767
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2007-05-24 13:17:50 +0000

    Exposed service layer through WCF

--- Source/Application/Services/Interfaces/IMediaService.cs
+++ Source/Application/Services/Interfaces/IMediaService.cs
@@ -1,5 +1,6 @@
 
 using System.Collections.Generic;
+using System.ServiceModel;
 
 using Atlanta.Application.Domain.Lender;
 
@@ -9,30 +10,35 @@ namespace Atlanta.Application.Services.Interfaces
     /// <summary>
     /// Interface for Media service
     /// </summary>
+    [ServiceContract()]
     public interface IMediaService : IServiceBase
     {
 
         /// <summary>
         ///  Get a list of Media for the system Library
         /// </summary>
+        [OperationContract]
         IList<Media> GetMediaList(  User            user,
                                     MediaCriteria   criteria);
 
         /// <summary>
         ///  Create a Media in the system Library
         /// </summary>
+        [OperationContract]
         Media Create(   User    user,
                         Media   orphanedMedia);
 
         /// <summary>
         ///  Modify an existing Media.
         /// </summary>
+        [OperationContract]
         Media Modify(   User    user,
                         Media   modifiedMediaCopy);
 
         /// <summary>
         ///  Delete the media from the system Library.
         /// </summary>
+        [OperationContract]
         void Delete(User    user,
                     Media   mediaCopy);
 
