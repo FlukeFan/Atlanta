c8276828d0243ff58351d6bcee3b43f5f3d88001
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2009-01-19 14:40:49 +0000

    Initial Silverlight checkin.

---
+++ Source/Presentation/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Browser;
+
+namespace Atlanta.Presentation
+{
+
+    public class App : Application
+    {
+
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+            this.Exit += this.Application_Exit;
+            this.UnhandledException += this.Application_UnhandledException;
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Uri uri = new Uri("Main.xaml", UriKind.Relative);
+
+            // Load the main control
+            Main main = new Main();
+            System.Windows.Application.LoadComponent(main, uri);
+            RootVisual = main;
+        }
+
+        private void Application_Exit(object sender, EventArgs e)
+        {
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            HtmlPage.Window.Alert(e.ExceptionObject.ToString());
+        }
+
+    }
+}
---
+++ Source/Presentation/Main.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Atlanta.Presentation
+{
+
+    public class Main : UserControl
+    {
+
+        public Main()
+        {
+            Loaded += new RoutedEventHandler(Main_Loaded);
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
