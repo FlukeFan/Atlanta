87277283dff6cce25890f9a42b2d44cc87f21e3f
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2009-01-21 16:30:30 +0000

    Added ServiceResult class to return values across SOAP.

---
+++ Source/Application/Services/ServiceBase/ServiceResult.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Atlanta.Application.Services.ServiceBase
+{
+
+    /// <summary>
+    /// Generic class for returning results from services (or wrapping an exception otherwise)
+    /// </summary>
+    [DataContract]
+    public class ServiceResult<T> : ServiceResult
+    {
+
+        /// <summary> protected constructor </summary>
+        protected ServiceResult() { }
+
+        /// <summary> return value </summary>
+        [DataMember]
+        public T Result { get; protected set; }
+
+        /// <summary> Create a return value </summary>
+        public static ServiceResult<T> Return(T result)
+        {
+            ServiceResult<T> serviceResult = new ServiceResult<T>();
+            serviceResult.Result = result;
+            return serviceResult;
+        }
+
+    }
+
+    /// <summary>
+    /// Wraps a return value from a service call, or an exception thrown from one.
+    /// </summary>
+    [DataContract]
+    public class ServiceResult
+    {
+
+        /// <summary> Protected constructor </summary>
+        protected ServiceResult()
+        {
+        }
+
+        /// <summary> is return value void </summary>
+        [DataMember] public bool IsVoid { get; protected set; }
+
+        /// <summary> is return value an error </summary>
+        [DataMember] public bool IsError { get; protected set; }
+
+        /// <summary> message to pass to the constructor of the exception </summary>
+        [DataMember] public string ExceptionMessage { get; protected set; }
+
+        /// <summary> class name of exception </summary>
+        [DataMember] public string ExceptionClass { get; protected set; }
+
+        /// <summary> dictionary of properties of the exception </summary>
+        [DataMember] public IDictionary<string, object> Properties { get; protected set; }
+
+        /// <summary>
+        /// Void service return value
+        /// </summary>
+        public static ServiceResult Void
+        {
+            get
+            {
+                ServiceResult serviceResult = new ServiceResult();
+                serviceResult.IsVoid = true;
+                return serviceResult;
+            }
+        }
+
+        /// <summary>
+        /// Wrap an exception to be returned from a service call
+        /// </summary>
+        public static ServiceResult Error(Exception exception)
+        {
+            ServiceResult serviceResult = new ServiceResult();
+            serviceResult.IsError = true;
+            serviceResult.ExceptionMessage = exception.Message;
+            serviceResult.ExceptionClass = exception.GetType().FullName;
+            serviceResult.Properties = new Dictionary<string, object>();
+
+            foreach (PropertyInfo property in exception.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                if (property.CanRead && property.CanWrite)
+                {
+                    serviceResult.Properties.Add(property.Name, property.GetValue(exception, null));
+                }
+            }
+
+            return serviceResult;
+        }
+
+    }
+
+}
