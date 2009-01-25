706fe2af39ae0fa3e094dc3fa5641f648b9265f4
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2009-01-25 13:07:28 +0000

    Added NUnit project to load all tests into NUnitGui.

--- NUnitGui.bat
+++ NUnitGui.bat
@@ -1,2 +1,1 @@
-@SET PATH=%CD%\SDKs\Firebird\bin;%CD%\SDKs\nant-0.86\bin\lib\common\2.0;%PATH%;
-start SDKs\nant-0.86\bin\lib\common\2.0\nunit-gui.exe
+start "AtlantaTests" SDKs\nant-0.86\bin\lib\common\2.0\nunit-gui.exe Atlanta.nunit
