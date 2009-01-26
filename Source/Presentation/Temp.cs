7521b5625343d73260d9eb922be158cf2eb248d1
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2009-01-26 11:44:33 +0000

    Added generation of client service call.

---
+++ Source/Presentation/Temp.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NHibernate.Criterion
+{
+
+    /// <summary>
+    /// Placeholder until alternative to NH DetachedCriteria is put in place
+    /// </summary>
+    public class DetachedCriteria
+	{
+	
+	}
+
+}
