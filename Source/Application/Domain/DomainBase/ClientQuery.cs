9e66f3b1d9c01a28f59b7af51efb3a5b5395c5db
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2009-01-28 15:05:40 +0000

    Added interface for adding expressions to client query.

--- Source/Application/Domain/DomainBase/ClientQuery.cs
+++ Source/Application/Domain/DomainBase/ClientQuery.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using System.Text;
 
 namespace Atlanta.Application.Domain.DomainBase
 {
+
+    /// <summary>
+    /// An expression from a client query
+    /// </summary>
+    [DataContract]
+    public class ClientQueryExpression
+    {
+        /// <summary> Target class of query </summary>
+        [DataMember]
+        public string Property { get; set; }
+
+        /// <summary> Target class of query </summary>
+        [DataMember]
+        public string Operator { get; set; }
+
+        /// <summary> Target class of query </summary>
+        [DataMember]
+        public object Operand { get; set; }
+    }
 
     /// <summary>
     /// Serialisable class for client query criteria
@@ -14,9 +34,19 @@ namespace Atlanta.Application.Domain.DomainBase
     public class ClientQuery
     {
 
+        /// <summary> Constructor </summary>
+        public ClientQuery()
+        {
+            Expressions = new List<ClientQueryExpression>();
+        }
+
         /// <summary> Target class of query </summary>
         [DataMember]
         public string ForClass { get; protected set; }
+
+        /// <summary> List of expressions </summary>
+        [DataMember]
+        public IList<ClientQueryExpression> Expressions { get; protected set; }
 
         /// <summary>
         /// Create a query for the given type
@@ -24,6 +54,14 @@ namespace Atlanta.Application.Domain.DomainBase
         public static ClientQuery For<T>()
         {
             return new ClientQuery() { ForClass = typeof(T).FullName };
+        }
+
+        /// <summary>
+        /// Add a lambda expression to the query
+        /// </summary>
+        public ClientQuery Add<T>(Expression<Func<T, bool>> expression)
+        {
+            return this;
         }
 
     }
--- Source/Application/Domain/DomainBase/Test/TestClientQuery.cs
+++ Source/Application/Domain/DomainBase/Test/TestClientQuery.cs
@@ -7,19 +7,29 @@ namespace Atlanta.Application.Domain.DomainBase.Test
     public class QueryClass
     {
         public string Name { get; set; }
+        public QueryClassRelation RelatedTo { get; set; }
+    }
+
+    public class QueryClassRelation : DomainObjectBase
+    {
     }
 
     [TestFixture]
     public class TestClientQuery : DomainTestBase
     {
 
-        [Test]
+        [Test] [Ignore("working on client expressions using Lambdas")]
         public void TestCreateQuery()
         {
             ClientQuery query =
-                ClientQuery.For<QueryClass>();
+                ClientQuery.For<QueryClass>()
+                    .Add((QueryClass q) => q.Name == "test name");
 
             Assert.AreEqual("Atlanta.Application.Domain.DomainBase.Test.QueryClass", query.ForClass);
+            Assert.AreEqual(1, query.Expressions.Count);
+            Assert.AreEqual("Name", query.Expressions[0].Property);
+            Assert.AreEqual("Eq", query.Expressions[0].Operator);
+            Assert.AreEqual("test name", query.Expressions[0].Operand);
         }
 
     }
