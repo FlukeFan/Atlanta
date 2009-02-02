13d30b6e496abc46d39e2299e6eddbb764c704d9
FlukeFan <FlukeFan@a9cdfca2-6323-0410-935c-25e3cb29754f>
2009-02-02 14:12:55 +0000

    Added initial stab at class to copy graphs of objects.

---
+++ Source/Application/Domain/DomainBase/Graph.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Atlanta.Application.Domain.DomainBase
+{
+
+    /// <summary>
+    /// Extension class to allow graphing of objects using fluent syntax
+    /// </summary>
+    public static class GraphExtensions
+    {
+
+        /// <summary>
+        /// Create a Graph&lt;T&gt; for an object"/>
+        /// </summary>
+        public static Graph<T> Graph<T>(this T source)
+        {
+            return new Graph<T>(source);
+        }
+
+    }
+
+    /// <summary>
+    /// Class to allow creation of copies of a graph of objects without circular references
+    /// </summary>
+    public class Graph<T>
+    {
+
+        private T _source;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Graph(T source)
+        {
+            _source = source;
+        }
+
+        /// <summary>
+        /// Return a copy of the selected graph of objects
+        /// </summary>
+        public T Copy()
+        {
+            T target = CreateBlankCopy();
+            CopyValues(target);
+
+            return target;
+        }
+
+        private void CopyValues(T target)
+        {
+            Type targetType = typeof(T);
+            while (targetType != null)
+            {
+                foreach (FieldInfo field in targetType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+                {
+                    if (field.FieldType.IsValueType
+                        || typeof(string).IsAssignableFrom(field.FieldType))
+                    {
+                        field.SetValue(target, field.GetValue(_source));
+                    }
+                }
+                targetType = targetType.BaseType;
+            }
+        }
+
+        private T CreateBlankCopy()
+        {
+            ConstructorInfo constructor =
+                typeof(T).GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+                                        null, new Type[] {}, null);
+
+            object copy = (T)constructor.Invoke(null);
+            return (T)copy;
+        }
+
+    }
+
+}
---
+++ Source/Application/Domain/DomainBase/Test/TestGraph.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Atlanta.Application.Domain.DomainBase.Test
+{
+
+    public class TestGraphRoot
+    {
+        public int Id { get; set; }
+    }
+
+    public class Parent : TestGraphRoot
+    {
+        public static Parent Create() { return new Parent(); }
+        protected Parent() { ChildList = new List<Child>(); }
+        public string Name { get; set; }
+        public IList<Child> ChildList { get; set; }
+        public Parent SetId(int id) { Id = id; return this; }
+        public Parent SetName(string name) { Name = name; return this; }
+    }
+
+    public class Child : TestGraphRoot
+    {
+        public Child() { Children = new List<Grandchild>(); }
+        public string Name { get; set; }
+        public Parent Parent { get; set; }
+        protected IList<Grandchild> Children { get; set; }
+        public IEnumerable ChildrenEnumeration { get { return Children; } }
+    }
+
+    public class Grandchild : TestGraphRoot
+    {
+        public string Name { get; set; }
+        public Child Parent { get; set; }
+    }
+
+
+    [TestFixture]
+    public class TestGraph : DomainTestBase
+    {
+
+        [Test]
+        public void TestCopySingleObject()
+        {
+            Parent parent = Parent.Create().SetId(1).SetName("parent1");
+
+            Parent parentCopy = parent.Graph().Copy();
+
+            Assert.AreNotSame(parent, parentCopy);
+            Assert.AreEqual(1, parentCopy.Id);
+            Assert.AreEqual("parent1", parentCopy.Name);
+            Assert.AreEqual(0, parentCopy.ChildList.Count);
+        }
+
+    }
+
+}
